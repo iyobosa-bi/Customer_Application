3645170987119320b59ca4cbd8426ca9899c669e
Blessing <iyobosa.isibor022@gmail.com>
2026-02-07 10:18:43 +0100

    add:created layout files and resource controller

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Customers</title>
-    <link rel="stylesheet" href="{{ asset('./assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('./assets/css/fontawesome.min.css') }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-
-</head>
+@section('content')
 
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
@@ -84,10 +74,11 @@
         </div>
     </div>
 </div>
-<script src="{{ asset('./assets/js/jquery.js') }}"></script>
-<script src="{{ asset('./assets/js/bootstrap.bundle.js') }}"></script>
-<script src="{{ asset('./assets/js/fontawesome.js') }}"></script>
 
-</body>
+@endsection
 
-</html>
+
+
+
+
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Customers</title>
+    <link rel="stylesheet" href="{{ asset('./assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('./assets/css/fontawesome.min.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+</head>
+    @yield('content')
+<script src="{{ asset('./assets/js/jquery.js') }}"></script>
+<script src="{{ asset('./assets/js/bootstrap.bundle.js') }}"></script>
+<script src="{{ asset('./assets/js/fontawesome.js') }}"></script>
+
+</body>
+
+</html>
